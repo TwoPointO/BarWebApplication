5312ab9a1a908b46933dafcdfa8829da54146e79
Marko Curic <mcuric1710@gmail.com>
2020-08-17 22:09:11 +0200

    Some small rework

--- Models/WebAppContext.cs
+++ Models/WebAppContext.cs
@@ -30,7 +30,7 @@ namespace BarWebApplication.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:barwebapp.database.windows.net,1433;Initial Catalog=WebApp;Persist Security Info=False;User ID=mcuric1710;Password=Thuglife1710!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("DeployedApp");
             }
         }
 
